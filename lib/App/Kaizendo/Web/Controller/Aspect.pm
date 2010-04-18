78ba4635b21943b06a415568644f0c8dc6155f69
Tomas Doran <bobtfish@bobtfish.net>
2010-04-18 20:34:08 +0100

    And I really don't want to write hello world URI templates for every method, ergo cheat

--- lib/App/Kaizendo/Web/Controller/Aspect.pm
+++ lib/App/Kaizendo/Web/Controller/Aspect.pm
@@ -2,7 +2,9 @@ package App::Kaizendo::Web::Controller::Aspect;
 use Moose;
 use namespace::autoclean;
 
-BEGIN { extends 'App::Kaizendo::Web::ControllerBase::REST'; }
+BEGIN { extends 'App::Kaizendo::Web::ControllerBase::REST' }
+
+with 'App::Kaizendo::Web::ControllerRole::Prototype';
 
 sub base : Chained('/base') PathPart(';') CaptureArgs(0) {}
 
