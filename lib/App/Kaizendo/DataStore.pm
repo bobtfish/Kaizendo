7e01e3a9601edff490ad094d7603c424b448b8eb
Tomas Doran <bobtfish@bobtfish.net>
2010-05-01 21:57:58 +0000

    Add a Catalyst model to the application

--- lib/App/Kaizendo/DataStore.pm
+++ lib/App/Kaizendo/DataStore.pm
@@ -6,4 +6,11 @@ use namespace::autoclean;
 extends qw/ KiokuX::Model /;
 with qw/ KiokuDB::Role::API /;
 
+around BUILDARGS => sub {
+    my ($orig, $self) = (shift, shift);
+    my $args = $self->$orig(@_);
+    delete $args->{$_} for qw/ clear_leaks manage_scope model_class /; # Ewww
+    return $args;
+};
+
 __PACKAGE__->meta->make_immutable;
---
+++ lib/App/Kaizendo/Web/Model/Documents.pm
@@ -0,0 +1,17 @@
+package App::Kaizendo::Web::Model::Documents;
+use Moose;
+use MooseX::Types::LoadableClass qw/ClassName/;
+use namespace::autoclean;
+
+extends 'Catalyst::Model::KiokuDB';
+
+__PACKAGE__->config(
+    dsn => "dbi:SQLite:dbname=kiokudb.sqlite3",
+    manage_scope => 1,
+    clear_leaks => 1,
+    model_class => 'App::Kaizendo::DataStore',
+);
+
+has '+model_class' => ( isa => ClassName, coerce => 1 );
+
+__PACKAGE__->meta->make_immutable;
