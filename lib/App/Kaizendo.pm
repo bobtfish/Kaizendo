36d013e3ed66058cd3ca72f00711fd47481e7215
Salve J. Nilsen <sjn@kaizendo.org>
2010-06-27 21:43:36 +0200

    Set correct LICENSE info

--- lib/App/Kaizendo.pm
+++ lib/App/Kaizendo.pm
@@ -42,10 +42,19 @@ Copyright the above authors.
 
 =head1 LICENSE
 
-This library is free software. You can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License v3, AGPLv3.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-See L<http://opensource.org/licenses/agpl-v3.html> for details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see L<http://www.gnu.org/licenses/>.
+
 
 =cut
 
