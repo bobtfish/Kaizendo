4c87463ef9fcca6cbd08899e0e12c411e707b949
Salve J. Nilsen <sjn@kaizendo.org>
2010-06-27 21:08:58 +0200

    Bump version

--- lib/App/Kaizendo.pm
+++ lib/App/Kaizendo.pm
@@ -4,7 +4,7 @@ use 5.008_005;
 use strict;
 use warnings;
 
-our $VERSION = '0.000_001';
+our $VERSION = '0.000_002';
 $VERSION = eval $VERSION;
 
 1;
@@ -24,14 +24,21 @@ App::Kaizendo - A tool for collaborative authoring of polyscopic documents
 Kaizendo is a framework for discussing and improving texts that have
 alternative versions ("aspects") of itself.
 
+The application is currently under development, and isn't in a particularly
+usable state yet. If you want to help please consult the README file!
+
+If you want to learn more about the project, check out L<http://kaizendo.org/>.
+
 =head1 AUTHORS
 
 Salve J. Nilsen <sjn@kaizendo.org>
 Tomas Doran <bobtfish@bobtfish.net>
 
+
 =head1 COPYRIGHT
 
 Copyright the above authors.
+
 
 =head1 LICENSE
 
